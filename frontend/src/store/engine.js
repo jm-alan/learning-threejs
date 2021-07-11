0ada72e4da9d346c2d792f8659ff1b7380916187
Joshua <j.maxwell.alan@gmail.com>
2021-07-11 03:48:04 -0400

    create state slices for necessary engine components

--- frontend/src/store/engine.js
+++ frontend/src/store/engine.js
@@ -1,4 +1,22 @@
 const CANVAS = 'engine/CANVAS';
+const SCENE = 'engine/SCENE';
+const CAMERA = 'engine/CAMERA';
+const RENDERER = 'engine/RENDERER';
+
+export const SetScene = scene => ({
+  type: SCENE,
+  scene
+});
+
+export const SetCamera = camera => ({
+  type: CAMERA,
+  camera
+});
+
+export const SetRenderer = renderer => ({
+  type: RENDERER,
+  renderer
+});
 
 export const SetCanvas = canvas => ({
   type: CANVAS,
@@ -7,11 +25,17 @@ export const SetCanvas = canvas => ({
 
 export default function reducer (
   state = { mooring: null },
-  { type, canvas }
+  { type, canvas, scene, camera, renderer }
 ) {
   switch (type) {
     case CANVAS:
       return { ...state, canvas };
+    case SCENE:
+      return { ...state, scene };
+    case CAMERA:
+      return { ...state, camera };
+    case RENDERER:
+      return { ...state, renderer };
     default:
       return state;
   }
