d8d391a7a1e75c6832d8b7b4a0649ed7abdfd463
Joshua <j.maxwell.alan@gmail.com>
2021-07-11 04:53:46 -0400

    basic render engine

--- frontend/src/components/Engine/index.js
+++ frontend/src/components/Engine/index.js
@@ -1,13 +1,18 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import * as Three from 'three';
-import { BuildDefault, DestroyEngine, SetCamera, SetRenderer, SetScene } from '../../store/engine';
+import { BuildDefault, DestroyEngine, Render, SetCamera, SetRenderer, SetScene } from '../../store/engine';
 
 export default function Engine () {
   const dispatch = useDispatch();
 
   const canvas = useSelector(state => state.engine.canvas);
   const renderer = useSelector(state => state.engine.renderer);
+  const scene = useSelector(state => state.engine.scene);
+  const camera = useSelector(state => state.engine.camera);
+  const cameraX = useSelector(state => state.engine.cameraX);
+  const cameraY = useSelector(state => state.engine.cameraY);
+  const cameraZ = useSelector(state => state.engine.cameraZ);
 
   useEffect(() => {
     if (canvas) {
@@ -30,6 +35,18 @@ export default function Engine () {
       dispatch(BuildDefault());
     }
   }, [dispatch, renderer]);
+
+  useEffect(() => {
+    if (renderer && scene && camera) dispatch(Render());
+  }, [dispatch, scene, camera]);
+
+  useEffect(() => {
+    if (camera) {
+      camera.position.setX(cameraX);
+      camera.position.setY(cameraY);
+      camera.position.setZ(cameraZ);
+    }
+  }, [dispatch, camera, cameraX, cameraY, cameraZ]);
 
   return null;
 }
