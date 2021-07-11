c6d2ce194b9a336c85395ae81b2340ccd43ba9ff
Joshua <j.maxwell.alan@gmail.com>
2021-07-11 15:09:21 -0400

    disable backend for the time being

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,27 @@
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+// import { useEffect } from 'react';
+// import { useDispatch, useSelector } from 'react-redux';
 import { Route, Switch } from 'react-router-dom';
 
 import Canvas from './components/Canvas';
 import Torus from './components/Geometries/Torus';
 import PointTest from './components/Lights/PointTest';
-import csrfetch from './store/csrfetch';
-import { RestoreUser } from './store/session';
+// import csrfetch from './store/csrfetch';
+// import { RestoreUser } from './store/session';
 
 export default function App () {
-  const dispatch = useDispatch();
+  // const dispatch = useDispatch();
 
-  const loaded = useSelector(state => state.session.loaded);
+  // const loaded = useSelector(state => state.session.loaded);
 
-  useEffect(() => {
-    csrfetch.captureDispatch(dispatch);
-    csrfetch.restoreCSRF();
-    dispatch(RestoreUser());
-  }, [dispatch]);
+  // useEffect(() => {
+  //   csrfetch.captureDispatch(dispatch);
+  //   csrfetch.restoreCSRF();
+  //   dispatch(RestoreUser());
+  // }, [dispatch]);
 
-  return loaded && (
+  // ! RESTORE "loaded &&"
+
+  return (
     <>
       <Switch>
         <Route path='/'>
