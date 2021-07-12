9ef016f215aa7e4bd6aea477788b5d3d09567111
Joshua <j.maxwell.alan@gmail.com>
2021-07-12 12:47:40 -0400

    create intensity type

--- frontend/src/store/engine/ambientLights/types.js
+++ frontend/src/store/engine/ambientLights/types.js
@@ -1,3 +1,4 @@
 export const NEW = 'engine/AMBIENTLIGHTS/NEW';
 export const DESTROY = 'engine/AMBIENTLIGHTS/DESTROY';
 export const COLOR = 'engine/AMBIENTLIGHTS/COLOR';
+export const INTENSITY = 'engine/AMBIENTLIGHTS/INTENSITY';
