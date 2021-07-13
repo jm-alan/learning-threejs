c9c24cc05961a6598f5e6b975f0273d19c5d02f9
Joshua <j.maxwell.alan@gmail.com>
2021-07-12 20:11:28 -0400

    genericize pointLight and move functions into component

--- frontend/src/components/Lights/PointLight.js
+++ frontend/src/components/Lights/PointLight.js
@@ -1,60 +1,65 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
-import {
-  CreatePointLight,
-  // MovePointLightX,
-  // MovePointLightY,
-  MovePointLightZ
-} from '../../store/engine/pointLights/actions';
-import { AddToScene } from '../../store/engine/scene/actions';
+import { CreatePointLight } from '../../store/engine/pointLights/actions';
+import { AddToScene, RemoveFromScene } from '../../store/engine/scene/actions';
 
-export default function PointLight ({ key, color, intensity, initialPosition }) {
+export default function PointLight ({ objectKey, initialColor, initialPosition, initialIntensity, initialDistance, initialDecay }) {
   const dispatch = useDispatch();
 
-  const pointOne = useSelector(state => state.engine.pointLights.all.pointOne?.light);
-  const pointTwo = useSelector(state => state.engine.pointLights.all.pointTwo?.light);
-  const pointThree = useSelector(state => state.engine.pointLights.all.pointThree?.light);
-  const pointFour = useSelector(state => state.engine.pointLights.all.pointFour?.light);
-  const pointFive = useSelector(state => state.engine.pointLights.all.pointFive?.light);
-  const pointSix = useSelector(state => state.engine.pointLights.all.pointSix?.light);
   const ready = useSelector(state => state.engine.renderer.ready);
+  const light = useSelector(state => state.engine.pointLights.all[objectKey]);
+  const posX = useSelector(state => state.engine.pointLights.all[objectKey]?.posX);
+  const posY = useSelector(state => state.engine.pointLights.all[objectKey]?.posY);
+  const posZ = useSelector(state => state.engine.pointLights.all[objectKey]?.posZ);
+  const color = useSelector(state => state.engine.pointLights.all[objectKey]?.color);
+  const intensity = useSelector(state => state.engine.pointLights.all[objectKey]?.intensity);
+  const distance = useSelector(state => state.engine.pointLights.all[objectKey]?.distance);
+  const decay = useSelector(state => state.engine.pointLights.all[objectKey]?.decay);
 
   useEffect(() => {
     if (ready) {
-      // if (!pointOne) dispatch(CreatePointLight('pointOne', 0xFFFFFF));
-      // if (!pointTwo) dispatch(CreatePointLight('pointTwo', 0xFFFFFF));
-      // if (!pointThree) dispatch(CreatePointLight('pointThree', 0xFFFFFF));
-      // if (!pointFour) dispatch(CreatePointLight('pointFour', 0xFFFFFF));
-      if (!pointFive) dispatch(CreatePointLight('pointFive'));
-      if (!pointSix) dispatch(CreatePointLight('pointSix'));
+      if (!light) {
+        dispatch(CreatePointLight(
+          objectKey, initialColor, initialIntensity,
+          initialDistance, initialDecay, initialPosition
+        ));
+      }
     }
-  }, [dispatch, ready, pointOne, pointTwo, pointThree, pointFour, pointFive, pointSix]);
+  }, [dispatch, ready, light, objectKey, initialColor, initialPosition, initialIntensity, initialDistance, initialDecay]);
 
-  // useEffect(() => {
-  //   if (pointOne) dispatch(MovePointLightX.absolute('pointOne', -100));
-  // }, [dispatch, pointOne]);
-  // useEffect(() => {
-  //   if (pointTwo) dispatch(MovePointLightX.absolute('pointTwo', 100));
-  // }, [dispatch, pointTwo]);
-  // useEffect(() => {
-  //   if (pointThree) dispatch(MovePointLightY.absolute('pointThree', 100));
-  // }, [dispatch, pointThree]);
-  // useEffect(() => {
-  //   if (pointFour) dispatch(MovePointLightY.absolute('pointFour', -100));
-  // }, [dispatch, pointFour]);
   useEffect(() => {
-    if (pointFive) {
-      dispatch(AddToScene(pointFive));
-      dispatch(MovePointLightZ.absolute('pointFive', -100));
-    }
-  }, [dispatch, pointFive]);
+    if (ready && light) dispatch(AddToScene(light));
+    return () => !ready && light && dispatch(RemoveFromScene(light));
+  }, [dispatch, ready, light]);
+
   useEffect(() => {
-    if (pointSix) {
-      dispatch(AddToScene(pointSix));
-      dispatch(MovePointLightZ.absolute('pointSix', 100));
-    }
-  }, [dispatch, pointSix]);
+    if (light) light.object.position.setX(posX);
+  }, [light, posX]);
+
+  useEffect(() => {
+    if (light) light.object.position.setY(posY);
+  }, [light, posY]);
+
+  useEffect(() => {
+    if (light) light.object.position.setZ(posZ);
+  }, [light, posZ]);
+
+  useEffect(() => {
+    if (light) light.object.intensity = intensity;
+  }, [light, intensity]);
+
+  useEffect(() => {
+    if (light) light.object.distance = distance;
+  }, [light, distance]);
+
+  useEffect(() => {
+    if (light) light.object.decay = decay;
+  }, [light, decay]);
+
+  useEffect(() => {
+    if (light) light.object.color.set(color);
+  }, [light, color]);
 
   return null;
 }
