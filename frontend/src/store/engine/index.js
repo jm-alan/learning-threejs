c0669698e04222bec8590771bc41cd2e5fe12631
Joshua <j.maxwell.alan@gmail.com>
2021-07-13 13:53:52 -0400

    camera -> cameras

--- frontend/src/store/engine/index.js
+++ frontend/src/store/engine/index.js
@@ -2,7 +2,7 @@ import { combineReducers } from 'redux';
 
 import canvas from './canvas';
 import scene from './scene';
-import camera from './camera';
+import cameras from './cameras';
 import renderer from './renderer';
 import geometries from './geometries';
 import pointLights from './pointLights';
@@ -11,7 +11,7 @@ import keys from './keys';
 export default combineReducers({
   canvas,
   scene,
-  camera,
+  cameras,
   renderer,
   geometries,
   pointLights,
