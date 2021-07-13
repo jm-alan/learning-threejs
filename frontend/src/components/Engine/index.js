9f0ffaba82bc73c819c784d6bbb68baf12da6c69
Joshua <j.maxwell.alan@gmail.com>
2021-07-12 20:12:35 -0400

    remove unused code

--- frontend/src/components/Engine/index.js
+++ frontend/src/components/Engine/index.js
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { MoveCameraZ } from '../../store/engine/camera/actions';
-import { BuildDefault, CreateRenderer, DestroyRenderer, Render } from '../../store/engine/renderer/actions';
+import { BuildDefault, CreateRenderer, DestroyRenderer } from '../../store/engine/renderer/actions';
 
 import { CreateScene, DestroyScene } from '../../store/engine/scene/actions';
 
@@ -14,10 +14,6 @@ export default function Engine ({ children }) {
   const scene = useSelector(state => state.engine.scene.current);
   const camera = useSelector(state => state.engine.camera.current);
   const renderObjects = useSelector(state => state.engine.renderer.functions);
-  // const pointOne = useSelector(state => state.engine.pointLights.pointOne?.light);
-  // const pointTwo = useSelector(state => state.engine.pointLights.pointTwo?.light);
-  // const pointThree = useSelector(state => state.engine.pointLights.pointThree?.light);
-  // const pointFour = useSelector(state => state.engine.pointLights.pointFour?.light);
 
   useEffect(() => {
     if (canvas) {
@@ -40,7 +36,7 @@ export default function Engine ({ children }) {
   useEffect(() => {
     const animate = () => {
       if (scene && camera && ready) {
-        dispatch(Render(scene, camera));
+        renderer.render(scene, camera);
         for (const renderObj of renderObjects) renderObj.action();
       }
       return window.requestAnimationFrame(animate);
@@ -48,16 +44,6 @@ export default function Engine ({ children }) {
     const captureFrame = animate();
     return () => window.cancelAnimationFrame(captureFrame);
   }, [dispatch, scene, camera, ready, renderObjects]);
-
-  // useEffect(() => {
-  //   const interval = setInterval(() => {
-  //     if (pointOne) dispatch(SetLightColor('pointLight', 'pointOne', Math.round(Math.random() * 0xFFFFFF)));
-  //     if (pointTwo) dispatch(SetLightColor('pointLight', 'pointTwo', Math.round(Math.random() * 0xFFFFFF)));
-  //     if (pointThree) dispatch(SetLightColor('pointLight', 'pointThree', Math.round(Math.random() * 0xFFFFFF)));
-  //     if (pointFour) dispatch(SetLightColor('pointLight', 'pointFour', Math.round(Math.random() * 0xFFFFFF)));
-  //   }, 100);
-  //   return () => clearInterval(interval);
-  // }, [dispatch, pointOne, pointTwo, pointThree, pointFour]);
 
   return children;
 }
