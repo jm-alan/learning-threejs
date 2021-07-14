16bf966eeb44e5114f76298b1a9bf5814c1ca637
Joshua <j.maxwell.alan@gmail.com>
2021-07-14 01:07:34 -0400

    add debug menu enable to pause menu

--- frontend/src/components/Overlays/Pause.js
+++ frontend/src/components/Overlays/Pause.js
@@ -1,10 +1,17 @@
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
+import { HideDebugMenu, ShowDebugMenu } from '../../store/engine/overlays/actions';
 import { SetModalSize, SetModalPosition, ColorModalContent } from '../../store/modal';
 
 export default function Pause () {
   const dispatch = useDispatch();
+
+  const debugEnabled = useSelector(state => state.engine.overlays.debug);
+
+  const enableDebug = () => dispatch(ShowDebugMenu());
+
+  const disableDebug = () => dispatch(HideDebugMenu());
 
   useEffect(() => {
     dispatch(SetModalSize({ height: '90vh', width: '50vw' }));
@@ -19,9 +26,21 @@ export default function Pause () {
 
   return (
     <div id='pause'>
-      <h1>
-        The Game is Paused!
-      </h1>
+      {debugEnabled
+        ? (
+          <button
+            onClick={disableDebug}
+          >
+            Hide Debug Menu
+          </button>
+          )
+        : (
+          <button
+            onClick={enableDebug}
+          >
+            Show Debug Menu
+          </button>
+          )}
     </div>
   );
 }
