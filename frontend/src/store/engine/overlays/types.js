7f4a6bc679b45de814b95543d57ca6db61c1cd9a
Joshua <j.maxwell.alan@gmail.com>
2021-07-13 22:39:14 -0400

    create add/remove overlay types

--- frontend/src/store/engine/overlays/types.js
+++ frontend/src/store/engine/overlays/types.js
@@ -1,3 +1,5 @@
 export const SHOW_DEBUG = 'engine/OVERLAYS/DEBUG/SHOW';
 export const HIDE_DEBUG = 'engine/OVERLAYS/DEBUG/HIDE';
 export const MOORING = 'engine/OVERLAYS/MOORING';
+export const ADD = 'engine/OVERLAYS/ADD';
+export const REMOVE = 'engine/OVERLAYS/REMOVE';
