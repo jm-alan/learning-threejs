c651761b96a651431d84d8f1a3d65c210be9db8a
Joshua <j.maxwell.alan@gmail.com>
2021-07-13 22:20:05 -0400

    create overlay mooring case

--- frontend/src/store/engine/overlays/index.js
+++ frontend/src/store/engine/overlays/index.js
@@ -1,14 +1,16 @@
 import * as types from './types';
 
 export default function reducer (
-  state = { debug: false },
-  { type }
+  state = { debug: false, mooring: null },
+  { type, mooring }
 ) {
   switch (type) {
     case types.SHOW_DEBUG:
       return { ...state, debug: true };
     case types.HIDE_DEBUG:
       return { ...state, debug: false };
+    case types.MOORING:
+      return { ...state, mooring };
     default:
       return state;
   }
