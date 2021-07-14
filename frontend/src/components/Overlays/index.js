9ef4e69159d3b6bd823d3d0b0f925a08d3d1d78e
Joshua <j.maxwell.alan@gmail.com>
2021-07-14 01:07:21 -0400

    mount overlays and debug menu

--- frontend/src/components/Overlays/index.js
+++ frontend/src/components/Overlays/index.js
@@ -1,17 +1,25 @@
 import { useEffect } from 'react';
+import { createPortal } from 'react-dom';
 import { useDispatch, useSelector } from 'react-redux';
 
 import Pause from './Pause';
+import Debug from './Debug';
 import { SetModal, TearDown } from '../../store/modal';
 import { ShowModal, HideModal } from '../../store/UX';
 
 import './index.css';
+import { AddOverlay, RemoveOverlay } from '../../store/engine/overlays/actions';
 
 export default function Overlays () {
   const dispatch = useDispatch();
 
   const ready = useSelector(state => state.engine.renderer.ready);
   const paused = useSelector(state => state.engine.renderer.paused);
+  const current = useSelector(state => Object.values(state.engine.overlays.current));
+  const mooring = useSelector(state => state.engine.overlays.mooring);
+  const debugEnabled = useSelector(state => state.engine.overlays.debug);
+
+  const funcToComponent = (Element, idx) => <Element key={idx} />;
 
   useEffect(() => {
     if (ready && paused) {
@@ -24,5 +32,13 @@ export default function Overlays () {
     };
   }, [dispatch, ready, paused]);
 
-  return null;
+  useEffect(() => {
+    if (ready && debugEnabled) dispatch(AddOverlay('debug', Debug));
+    else if (ready && !debugEnabled) dispatch(RemoveOverlay('debug'));
+  }, [dispatch, ready, debugEnabled]);
+
+  return mooring && createPortal(
+    current.map(funcToComponent),
+    mooring
+  );
 }
