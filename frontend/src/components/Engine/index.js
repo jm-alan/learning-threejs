b313b005f8b853126608717a6256f905f4cbbda3
josh <j.maxwell.alan@gmail.com>
2021-07-17 11:20:43 -0400

    remove scene build from engine main

--- frontend/src/components/Engine/index.js
+++ frontend/src/components/Engine/index.js
@@ -2,7 +2,6 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { BuildDefault, CreateRenderer, DestroyRenderer } from '../../store/engine/renderer/actions';
-import { CreateScene, DestroyScene } from '../../store/engine/scene/actions';
 
 export default function Engine ({ children }) {
   const dispatch = useDispatch();
@@ -10,35 +9,33 @@ export default function Engine ({ children }) {
   const ready = useSelector(state => state.engine.renderer.ready);
   const canvas = useSelector(state => state.engine.canvas.current);
   const renderer = useSelector(state => state.engine.renderer.current);
-  const scene = useSelector(state => state.engine.scene.current);
+  const scene = useSelector(state => state.engine.scene.current.object);
   const camera = useSelector(state => state.engine.cameras.current.object);
   const renderObjects = useSelector(state => state.engine.renderer.functions);
   const paused = useSelector(state => state.engine.renderer.paused);
 
   useEffect(() => {
     if (canvas) {
-      dispatch(CreateScene());
       dispatch(CreateRenderer(canvas));
     }
     return () => {
       dispatch(DestroyRenderer());
-      dispatch(DestroyScene());
     };
   }, [dispatch, canvas]);
 
   useEffect(() => {
-    if (canvas && scene && camera && renderer) {
+    if (scene && camera && renderer) {
       dispatch(BuildDefault());
     }
   }, [dispatch, scene, camera, canvas, renderer]);
 
   useEffect(() => {
     const animate = () => {
-      if (scene && camera && renderer && ready && !paused) {
+      if (ready && !paused) {
         renderer.render(scene, camera);
         for (let i = 0; i < renderObjects.length; i++) renderObjects[i].action();
+        return window.requestAnimationFrame(animate);
       }
-      return window.requestAnimationFrame(animate);
     };
     const captureFrame = animate();
     return () => window.cancelAnimationFrame(captureFrame);
