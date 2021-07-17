634abaad6205ee62532b39b92b7867b9bd46d7dc
josh <j.maxwell.alan@gmail.com>
2021-07-17 01:40:18 -0400

    create positiona and rotation cases

--- frontend/src/store/engine/geometries/index.js
+++ frontend/src/store/engine/geometries/index.js
@@ -4,7 +4,11 @@ import * as types from './types';
 
 export default function reducer (
   state = { all: {} },
-  { type, name, props }
+  {
+    type, name, props,
+    posX, posY, posZ,
+    rotX, rotY, rotZ
+  }
 ) {
   switch (type) {
     case types.NEW:
@@ -31,6 +35,171 @@ export default function reducer (
         ...state,
         all: {
           ...state.all
+        }
+      };
+    case types.READY_POS:
+      return {
+        ...state,
+        all: {
+          ...state.all,
+          [name]: {
+            ...state.all[name],
+            readyPos: true
+          }
+        }
+      };
+    case types.READY_ROT:
+      return {
+        ...state,
+        all: {
+          ...state.all,
+          [name]: {
+            ...state.all[name],
+            readyRot: true
+          }
+        }
+      };
+    case types.READY:
+      return {
+        ...state,
+        all: {
+          ...state.all,
+          [name]: {
+            ...state.all[name],
+            ready: true
+          }
+        }
+      };
+    case types.POSX_ABSOLUTE:
+      return {
+        ...state,
+        all: {
+          ...state.all,
+          [name]: {
+            ...state.all[name],
+            posX
+          }
+        }
+      };
+    case types.POSY_ABSOLUTE:
+      return {
+        ...state,
+        all: {
+          ...state.all,
+          [name]: {
+            ...state.all[name],
+            posY
+          }
+        }
+      };
+    case types.POSZ_ABSOLUTE:
+      return {
+        ...state,
+        all: {
+          ...state.all,
+          [name]: {
+            ...state.all[name],
+            posZ
+          }
+        }
+      };
+    case types.POSX_RELATIVE:
+      return {
+        ...state,
+        all: {
+          ...state.all,
+          [name]: {
+            ...state.all[name],
+            posX: state.all[name].posX + posX
+          }
+        }
+      };
+    case types.POSY_RELATIVE:
+      return {
+        ...state,
+        all: {
+          ...state.all,
+          [name]: {
+            ...state.all[name],
+            posY: state.all[name].posY + posY
+          }
+        }
+      };
+    case types.POSZ_RELATIVE:
+      return {
+        ...state,
+        all: {
+          ...state.all,
+          [name]: {
+            ...state.all[name],
+            posZ: state.all[name].posZ + posZ
+          }
+        }
+      };
+    case types.ROTX_ABSOLUTE:
+      return {
+        ...state,
+        all: {
+          ...state.all,
+          [name]: {
+            ...state.all[name],
+            rotX
+          }
+        }
+      };
+    case types.ROTY_ABSOLUTE:
+      return {
+        ...state,
+        all: {
+          ...state.all,
+          [name]: {
+            ...state.all[name],
+            rotY
+          }
+        }
+      };
+    case types.ROTZ_ABSOLUTE:
+      return {
+        ...state,
+        all: {
+          ...state.all,
+          [name]: {
+            ...state.all[name],
+            rotZ
+          }
+        }
+      };
+    case types.ROTX_RELATIVE:
+      return {
+        ...state,
+        all: {
+          ...state.all,
+          [name]: {
+            ...state.all[name],
+            rotX: state.all[name].rotX + rotX
+          }
+        }
+      };
+    case types.ROTY_RELATIVE:
+      return {
+        ...state,
+        all: {
+          ...state.all,
+          [name]: {
+            ...state.all[name],
+            rotY: state.all[name].rotY + rotY
+          }
+        }
+      };
+    case types.ROTZ_RELATIVE:
+      return {
+        ...state,
+        all: {
+          ...state.all,
+          [name]: {
+            ...state.all[name],
+            rotZ: state.all[name].rotZ + rotZ
+          }
         }
       };
     default:
