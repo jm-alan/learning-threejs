04d3cb781f29b2dc5ef660870ce50bf98e1b776a
josh <j.maxwell.alan@gmail.com>
2021-07-17 11:14:30 -0400

    adjust scene reducer to support multiple objects

--- frontend/src/store/engine/scene/index.js
+++ frontend/src/store/engine/scene/index.js
@@ -2,23 +2,64 @@ import * as Three from 'three';
 
 import * as types from './types';
 
+const initialScene = {
+  name: null,
+  object: null
+};
+
 export default function reducer (
-  state = { current: null },
-  { type, object }
+  state = {
+    called: null,
+    current: { ...initialScene },
+    all: {}
+  },
+  { type, object, name }
 ) {
   switch (type) {
+    case types.CURRENT:
+      return {
+        ...state,
+        current: {
+          name,
+          object: state.all[name]
+        }
+      };
     case types.CREATE:
       return {
         ...state,
-        current: new Three.Scene()
+        all: {
+          ...state.all,
+          [name]: new Three.Scene()
+        }
       };
     case types.DESTROY:
-      return { ...state, current: null };
+      delete state.all[name];
+      if (name === state.current.name) {
+        delete state.current.object;
+        delete state.current.name;
+        delete state.current;
+        return {
+          ...state,
+          called: null,
+          current: {
+            ...initialScene
+          },
+          all: {
+            ...state.all
+          }
+        };
+      }
+      return {
+        ...state,
+        all: {
+          ...state.all
+        }
+      };
     case types.ADD:
-      state.current.add(object);
+      state.current.object.add(object);
       return state;
     case types.REMOVE:
-      state.current.remove(object);
+      state.current.object.remove(object);
       return state;
     default:
       return state;
