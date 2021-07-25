1fda7dfa66879e2c7223a8191220106d6eb112b1
Joshua <j.maxwell.alan@gmail.com>
2021-07-24 21:24:34 -0400

    refactor movement handler component to use useRenderer

--- frontend/src/components/Camera/Movement.js
+++ frontend/src/components/Camera/Movement.js
@@ -1,12 +1,13 @@
 import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
-import { AddRenderFunction, RemoveRenderFunction } from '../../store/engine/renderer/actions';
 import { MoveCameraX, MoveCameraY, MoveCameraZ } from '../../store/engine/cameras/actions';
+import { useRenderer } from '../../utils/hooks';
 
 export default function Movement () {
   const dispatch = useDispatch();
 
+  const ready = useSelector(state => state.engine.renderer.ready);
   const W = useSelector(state => state.engine.keys.pressed.KeyW);
   const A = useSelector(state => state.engine.keys.pressed.KeyA);
   const S = useSelector(state => state.engine.keys.pressed.KeyS);
@@ -15,53 +16,55 @@ export default function Movement () {
   const Space = useSelector(state => state.engine.keys.pressed.Space);
   const calledCamera = useSelector(state => state.engine.cameras.called);
 
+  const renderer = useRenderer();
+
   useEffect(() => {
-    if (calledCamera) {
+    if (ready && calledCamera) {
       const moveForward = () => dispatch(MoveCameraZ.relative(calledCamera, -1));
-      if (W) dispatch(AddRenderFunction('forward', moveForward));
-      if (!W) dispatch(RemoveRenderFunction('forward'));
+      if (W) renderer.addFunction('forward', moveForward);
+      if (!W) renderer.removeFunction('forward');
     }
-  }, [dispatch, calledCamera, W]);
+  }, [dispatch, ready, calledCamera, W]);
 
   useEffect(() => {
-    if (calledCamera) {
+    if (ready && calledCamera) {
       const moveBackward = () => dispatch(MoveCameraZ.relative(calledCamera, 1));
-      if (S) dispatch(AddRenderFunction('backward', moveBackward));
-      if (!S) dispatch(RemoveRenderFunction('backward'));
+      if (S) renderer.addFunction('backward', moveBackward);
+      if (!S) renderer.removeFunction('backward');
     }
-  }, [dispatch, calledCamera, S]);
+  }, [dispatch, ready, calledCamera, S]);
 
   useEffect(() => {
-    if (calledCamera) {
+    if (ready && calledCamera) {
       const moveLeft = () => dispatch(MoveCameraX.relative(calledCamera, -1));
-      if (A) dispatch(AddRenderFunction('left', moveLeft));
-      if (!A) dispatch(RemoveRenderFunction('left'));
+      if (A) renderer.addFunction('left', moveLeft);
+      if (!A) renderer.removeFunction('left');
     }
-  }, [dispatch, calledCamera, A]);
+  }, [dispatch, ready, calledCamera, A]);
 
   useEffect(() => {
-    if (calledCamera) {
+    if (ready && calledCamera) {
       const moveRight = () => dispatch(MoveCameraX.relative(calledCamera, 1));
-      if (D) dispatch(AddRenderFunction('right', moveRight));
-      if (!D) dispatch(RemoveRenderFunction('right'));
+      if (D) renderer.addFunction('right', moveRight);
+      if (!D) renderer.removeFunction('right');
     }
-  }, [dispatch, calledCamera, D]);
+  }, [dispatch, ready, calledCamera, D]);
 
   useEffect(() => {
-    if (calledCamera) {
+    if (ready && calledCamera) {
       const moveUp = () => dispatch(MoveCameraY.relative(calledCamera, 1));
-      if (Space) dispatch(AddRenderFunction('up', moveUp));
-      if (!Space) dispatch(RemoveRenderFunction('up'));
+      if (Space) renderer.addFunction('up', moveUp);
+      if (!Space) renderer.removeFunction('up');
     }
-  }, [dispatch, calledCamera, Space]);
+  }, [dispatch, ready, calledCamera, Space]);
 
   useEffect(() => {
-    if (calledCamera) {
+    if (ready && calledCamera) {
       const moveDown = () => dispatch(MoveCameraY.relative(calledCamera, -1));
-      if (ShiftLeft) dispatch(AddRenderFunction('down', moveDown));
-      if (!ShiftLeft) dispatch(RemoveRenderFunction('down'));
+      if (ShiftLeft) renderer.addFunction('down', moveDown);
+      if (!ShiftLeft) renderer.removeFunction('down');
     }
-  }, [dispatch, calledCamera, ShiftLeft]);
+  }, [dispatch, ready, calledCamera, ShiftLeft]);
 
   return null;
 }
