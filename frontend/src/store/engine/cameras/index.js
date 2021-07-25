acbe9f1114da237c889967dc2cea8d99116f5e75
Joshua <j.maxwell.alan@gmail.com>
2021-07-24 22:29:54 -0400

    remove now unused camera position/rotation types

--- frontend/src/store/engine/cameras/index.js
+++ frontend/src/store/engine/cameras/index.js
@@ -13,12 +13,7 @@ export default function reducer (
     functions: {},
     keys: []
   },
-  {
-    type, props, name,
-    posX, posY, posZ,
-    rotX, rotY, rotZ,
-    action, object
-  }
+  { type, props, name, action, object }
 ) {
   switch (type) {
     case types.CALL:
@@ -113,42 +108,6 @@ export default function reducer (
           }
         }
       };
-    case types.POSX_ABSOLUTE:
-      state.all[name].posX = posX;
-      return state;
-    case types.POSY_ABSOLUTE:
-      state.all[name].posY = posY;
-      return state;
-    case types.POSZ_ABSOLUTE:
-      state.all[name].posZ = posZ;
-      return state;
-    case types.POSX_RELATIVE:
-      state.all[name].posX += posX;
-      return state;
-    case types.POSY_RELATIVE:
-      state.all[name].posY += posY;
-      return state;
-    case types.POSZ_RELATIVE:
-      state.all[name].posZ += posZ;
-      return state;
-    case types.ROTX_ABSOLUTE:
-      state.all[name].rotX = rotX;
-      return state;
-    case types.ROTY_ABSOLUTE:
-      state.all[name].rotY = rotY;
-      return state;
-    case types.ROTZ_ABSOLUTE:
-      state.all[name].rotZ = rotZ;
-      return state;
-    case types.ROTX_RELATIVE:
-      state.all[name].rotX += rotX;
-      return state;
-    case types.ROTY_RELATIVE:
-      state.all[name].rotY += rotY;
-      return state;
-    case types.ROTZ_RELATIVE:
-      state.all[name].rotZ += rotZ;
-      return state;
     default:
       return state;
   }
