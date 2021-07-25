710fcf0d30732a174ebb212a3162a6bf5c3fa1ce
Joshua <j.maxwell.alan@gmail.com>
2021-07-24 22:19:19 -0400

    directly rotate geometries rather than dispatch

--- frontend/src/components/Geometries/Animations/Rotate.js
+++ frontend/src/components/Geometries/Animations/Rotate.js
@@ -2,25 +2,25 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { useRenderer } from '../../../utils/hooks';
-import { RotGeometryX, RotGeometryY, RotGeometryZ } from '../../../store/engine/geometries/actions';
 
 export default function Rotate ({ objectKey, name, rotX = 0, rotY = 0, rotZ = 0 }) {
   const dispatch = useDispatch();
 
   const renderReady = useSelector(state => state.engine.renderer.ready);
+  const object = useSelector(state => state.engine.geometries.all[objectKey]?.object);
   const objectReady = useSelector(state => state.engine.geometries.all[objectKey]?.ready);
   const renderer = useRenderer();
 
   useEffect(() => {
     if (renderReady && objectReady) {
       renderer.addFunction(name, () => {
-        dispatch(RotGeometryX.relative(objectKey, rotX));
-        dispatch(RotGeometryY.relative(objectKey, rotY));
-        dispatch(RotGeometryZ.relative(objectKey, rotZ));
+        object.rotation.x += rotX;
+        object.rotation.y += rotY;
+        object.rotation.z += rotZ;
       });
     }
     return () => renderer.removeFunction(name);
-  }, [dispatch, renderer, renderReady, objectReady, name, objectKey, rotX, rotY, rotZ]);
+  }, [dispatch, object, renderer, renderReady, objectReady, name, objectKey, rotX, rotY, rotZ]);
 
   return null;
 }
