84e2ddd162b0e1c08157fd0f6c1e4feb7a789b2d
Joshua <j.maxwell.alan@gmail.com>
2021-07-24 20:22:20 -0400

    create explicit renderer ready/unready cases

--- frontend/src/store/engine/renderer/index.js
+++ frontend/src/store/engine/renderer/index.js
@@ -12,32 +12,10 @@ export default function reducer (
   { type, object, name, action }
 ) {
   switch (type) {
-    case types.CREATE:
-      return {
-        ...state,
-        current: object
-      };
-    case types.DESTROY:
-      return { ...state, current: null, ready: false };
-    case types.BUILD_DEFAULT:
-      state.current.setPixelRatio(window.devicePixelRatio);
-      state.current.setSize(window.innerWidth, window.innerHeight);
+    case types.READY:
       return { ...state, ready: true };
-    case types.ADD_FUNCTION:
-      state.functions[name] = action;
-      state.keys.push(name);
-      return state;
-    case types.REMOVE_FUNCTION:
-      delete state.functions[name];
-      state.keys.splice(0, state.keys.length, ...Object.keys(state.functions));
-      return state;
-    case types.DUMP_FUNCTIONS:
-      state.functions = {};
-      state.keys.splice(0, state.keys.length);
-      return state;
-    case types.DUMP_LISTS:
-      state.current.renderLists.dispose();
-      return state;
+    case types.UNREADY:
+      return { ...state, ready: false };
     case types.PAUSE:
       return {
         ...state,
