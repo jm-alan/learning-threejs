43cc270634efd09609cbc77397171812fcb2342a
Joshua <j.maxwell.alan@gmail.com>
2021-07-24 21:25:47 -0400

    refactor rotate to use useRenderer

--- frontend/src/components/Geometries/Animations/Rotate.js
+++ frontend/src/components/Geometries/Animations/Rotate.js
@@ -1,24 +1,28 @@
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { RotGeometryX, RotGeometryY, RotGeometryZ } from '../../../store/engine/geometries/actions';
 
-import { AddRenderFunction, RemoveRenderFunction } from '../../../store/engine/renderer/actions';
+import { useRenderer } from '../../../utils/hooks';
+import { RotGeometryX, RotGeometryY, RotGeometryZ } from '../../../store/engine/geometries/actions';
 
 export default function Rotate ({ objectKey, name, rotX = 0, rotY = 0, rotZ = 0 }) {
   const dispatch = useDispatch();
 
+  const renderReady = useSelector(state => state.engine.renderer.ready);
   const objectReady = useSelector(state => state.engine.geometries.all[objectKey]?.ready);
+  const renderer = useRenderer();
+
+  console.log(React.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED);
 
   useEffect(() => {
-    if (objectReady) {
-      dispatch(AddRenderFunction(name, () => {
+    if (renderReady && objectReady) {
+      renderer.addFunction(name, () => {
         dispatch(RotGeometryX.relative(objectKey, rotX));
         dispatch(RotGeometryY.relative(objectKey, rotY));
         dispatch(RotGeometryZ.relative(objectKey, rotZ));
-      }));
+      });
     }
-    return () => dispatch(RemoveRenderFunction(name));
-  }, [dispatch, objectReady, name, objectKey, rotX, rotY, rotZ]);
+    return () => renderer.removeFunction(name);
+  }, [dispatch, renderer, renderReady, objectReady, name, objectKey, rotX, rotY, rotZ]);
 
   return null;
 }
