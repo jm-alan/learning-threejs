8471a003245871bbe211f54d2d0b2d26275349aa
Joshua <j.maxwell.alan@gmail.com>
2021-07-24 20:21:19 -0400

    create explicit renderer ready/unready action creators

--- frontend/src/store/engine/renderer/actions.js
+++ frontend/src/store/engine/renderer/actions.js
@@ -1,30 +1,11 @@
 import * as types from './types';
 
-export const BuildDefault = () => ({
-  type: types.BUILD_DEFAULT
+export const ReadyRenderer = () => ({
+  type: types.READY
 });
 
-export const CreateRenderer = canvas => async dispatch => {
-  const { WebGLRenderer } = await import('three');
-  dispatch({
-    type: types.CREATE,
-    object: new WebGLRenderer({ canvas })
-  });
-};
-
-export const DestroyRenderer = () => ({
-  type: types.DESTROY
-});
-
-export const AddRenderFunction = (name, action) => ({
-  type: types.ADD_FUNCTION,
-  name,
-  action
-});
-
-export const RemoveRenderFunction = name => ({
-  type: types.REMOVE_FUNCTION,
-  name
+export const UnreadyRenderer = () => ({
+  type: types.UNREADY
 });
 
 export const PauseRender = () => ({
@@ -33,20 +14,4 @@ export const PauseRender = () => ({
 
 export const ResumeRender = () => ({
   type: types.UNPAUSE
-});
-
-export const DumpRenderFunctions = () => ({
-  type: types.DUMP_FUNCTIONS
-});
-
-export const DumpRenderLists = () => ({
-  type: types.DUMP_LISTS
-});
-
-export const MarkFunctionsChanged = () => ({
-  type: types.CHANGED
-});
-
-export const MarkFunctionsUnchanged = () => ({
-  type: types.UNCHANGED
 });
