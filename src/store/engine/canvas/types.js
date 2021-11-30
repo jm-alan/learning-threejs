3ce500d27ba5c09609447250667f8802cb842bd4
JM <j.maxwell.alan@gmail.com>
2021-11-30 15:59:12 -0500

    create pointer lock types

--- src/store/engine/canvas/types.js
+++ src/store/engine/canvas/types.js
@@ -1,2 +1,4 @@
 export const SET = 'engine/CANVAS/SET';
 export const UNSET = 'engine/CANVAS/UNSET';
+export const LOCK_POINTER = 'engine/CANVAS/LOCK_POINTER';
+export const UNLOCK_POINTER = 'engine/CANVAS/UNLOCK_POINTER';
